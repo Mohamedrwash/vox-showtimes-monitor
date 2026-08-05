d02b9a71b68fa63dc5998851d8901caba9e6df91
Mohamedrwash <mohamedrwash51@gmail.com>
2026-08-05 12:58:04 +0300

    config: enable Supabase in site config

--- site/js/config.js
+++ site/js/config.js
@@ -3,6 +3,6 @@
 // table are what protect the data, not this key. Fill these in after
 // creating the project (docs/supabase-schema.sql).
 window.VOXWATCH_SUPABASE = {
-  url: '',
-  anonKey: ''
+  url: 'https://plueqjsdnhmjtlqmmwpy.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsdWVxanNkbmhtanRscW1td3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5MTc2NjAsImV4cCI6MjEwMTQ5MzY2MH0.eccK22ENNeuMa6fLVgYXJmOtpZJH4UHQqpBx3SnTLM8'
 };
